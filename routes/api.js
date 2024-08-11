1b8d32876219e31a81a5c6f1c317c8899b243d89
Jan Konieczek <koleks92@gmail.com>
2024-08-11 09:15:40 +0000

    feat: api routes

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,37 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      const text = req.body.text;
+      const locale = req.body.locale;
+
+      if (text === "") {
+        res.json({ error: 'No text to translate'})
+      }
+      
+      if (!text || !locale) {
+        res.json({ error: 'Required field(s) missing'})
+      }
+
+
+
+
+      let translation
+      if (locale == "american-to-british") {
+        translation = translator.a2b(text);
+      } else if (locale == "british-to-american") {
+        translation = translator.b2a(text);
+      } else {
+        res.json({ error: 'Invalid value for locale field' })
+      }
+
+      if (text === translation) {
+        translation = "Everything looks good to me!"
+      }
+
+      res.json({
+        text: text,
+        translation: translation
+      })
       
     });
 };
