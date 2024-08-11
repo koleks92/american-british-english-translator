abc5ebd22c624f908533ac246a8d66b624a7eb5d
Jan Konieczek <koleks92@gmail.com>
2024-08-11 09:15:13 +0000

    feat: hightlight tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -8,66 +8,84 @@ const translator = new Translator;
 suite('Unit Tests', () => {
     suite('American to British', function() {
         test('Mangoes are my favorite fruit.', function() {
-            assert.equal(translator.a2b('Mangoes are my favorite fruit.'), 'Mangoes are my favourite fruit.')
+            assert.equal(translator.a2b('Mangoes are my favorite fruit.'), 'Mangoes are my <span class="highlight">favourite</span> fruit.')
         });
         test('I ate yogurt for breakfast.', function() {
-            assert.equal(translator.a2b('I ate yogurt for breakfast.'), 'I ate yoghurt for breakfast.')
+            assert.equal(translator.a2b('I ate yogurt for breakfast.'), 'I ate <span class="highlight">yoghurt</span> for breakfast.')
         });
         test("We had a party at my friend's condo.", function() {
-            assert.equal(translator.a2b("We had a party at my friend's condo."), "We had a party at my friend's flat.")
+            assert.equal(translator.a2b("We had a party at my friend's condo."), "We had a party at my friend's <span class=\"highlight\">flat</span>.")
         });
         test("Can you toss this in the trashcan for me?", function() {
-            assert.equal(translator.a2b("Can you toss this in the trashcan for me?"), "Can you toss this in the bin for me?")
+            assert.equal(translator.a2b("Can you toss this in the trashcan for me?"), 'Can you toss this in the <span class="highlight">bin</span> for me?')
         });
         test("The parking lot was full.", function() {
-            assert.equal(translator.a2b("The parking lot was full."), "The car park was full.")
+            assert.equal(translator.a2b("The parking lot was full."), 'The <span class="highlight">car park</span> was full.')
         });
         test("Like a high tech Rube Goldberg machine.", function() {
-            assert.equal(translator.a2b("Like a high tech Rube Goldberg machine."), "Like a high tech Heath Robinson device.")
+            assert.equal(translator.a2b("Like a high tech Rube Goldberg machine."), 'Like a high tech <span class="highlight">Heath Robinson device</span>.')
         });
         test("To play hooky means to skip class or work.", function() {
-            assert.equal(translator.a2b("To play hooky means to skip class or work."), "To bunk off means to skip class or work.")
+            assert.equal(translator.a2b("To play hooky means to skip class or work."), 'To <span class="highlight">bunk off</span> means to skip class or work.')
         });
         test("No Mr. Bond, I expect you to die.", function() {
-            assert.equal(translator.a2b("No Mr. Bond, I expect you to die."), "No Mr Bond, I expect you to die.")
+            assert.equal(translator.a2b("No Mr. Bond, I expect you to die."), 'No <span class="highlight">Mr</span> Bond, I expect you to die.')
         });
         test("Dr. Grosh will see you now.", function() {
-            assert.equal(translator.a2b("Dr. Grosh will see you now."), "Dr Grosh will see you now.")
+            assert.equal(translator.a2b("Dr. Grosh will see you now."), '<span class="highlight">Dr</span> Grosh will see you now.')
         });
         test("Lunch is at 12:15 today.", function() {
-            assert.equal(translator.a2b("Lunch is at 12:15 today."), "Lunch is at 12.15 today.")
+            assert.equal(translator.a2b("Lunch is at 12:15 today."), 'Lunch is at <span class="highlight">12.15</span> today.')
         });
     })
     suite('British to American', function() {
         test('We watched the footie match for a while.', function() {
-            assert.equal(translator.b2a('We watched the footie match for a while.'), 'We watched the soccer match for a while.')
+            assert.equal(translator.b2a('We watched the footie match for a while.'), 'We watched the <span class="highlight">soccer</span> match for a while.')
         });
         test('Paracetamol takes up to an hour to work.', function() {
-            assert.equal(translator.b2a('Paracetamol takes up to an hour to work.'), 'Tylenol takes up to an hour to work.')
+            assert.equal(translator.b2a('Paracetamol takes up to an hour to work.'), '<span class="highlight">Tylenol</span> takes up to an hour to work.')
         });
         test("First, caramelise the onions.", function() {
-            assert.equal(translator.b2a("First, caramelise the onions."), "First, caramelize the onions.")
+            assert.equal(translator.b2a("First, caramelise the onions."), "First, <span class=\"highlight\">caramelize</span> the onions.")
         });
         test("I spent the bank holiday at the funfair.", function() {
-            assert.equal(translator.b2a("I spent the bank holiday at the funfair."), "I spent the public holiday at the carnival.")
+            assert.equal(translator.b2a("I spent the bank holiday at the funfair."), "I spent the <span class=\"highlight\">public holiday</span> at the <span class=\"highlight\">carnival</span>.")
         });
         test("I had a bicky then went to the chippy.", function() {
-            assert.equal(translator.b2a("I had a bicky then went to the chippy."), "I had a cookie then went to the fish-and-fish-and-chip shop.")
+            assert.equal(translator.b2a("I had a bicky then went to the chippy."), "I had a <span class=\"highlight\">cookie</span> then went to the <span class=\"highlight\">fish-and-<span class=\"highlight\">fish-and-chip shop</span></span>.")
         });
         test("I've just got bits and bobs in my bum bag.", function() {
-            assert.equal(translator.b2a("I've just got bits and bobs in my bum bag."), "I've just got odds and ends in my fanny pack.")
+            assert.equal(translator.b2a("I've just got bits and bobs in my bum bag."), "I've just got <span class=\"highlight\">odds and ends</span> in my <span class=\"highlight\">fanny pack</span>.")
         });
         test("The car boot sale at Boxted Airfield was called off.", function() {
-            assert.equal(translator.b2a("The car boot sale at Boxted Airfield was called off."), "The swap meet at Boxted Airfield was called off.")
+            assert.equal(translator.b2a("The car boot sale at Boxted Airfield was called off."), "The <span class=\"highlight\">swap meet</span> at Boxted Airfield was called off.")
         });
         test("Have you met Mrs Kalyani?", function() {
-            assert.equal(translator.b2a("Have you met Mrs Kalyani?"), "Have you met Mrs. Kalyani?")
+            assert.equal(translator.b2a("Have you met Mrs Kalyani?"), "Have you met <span class=\"highlight\">Mrs.</span> Kalyani?")
         });
         test("Prof Joyner of King's College, London.", function() {
-            assert.equal(translator.b2a("Prof Joyner of King's College, London."), "Prof. Joyner of King's College, London.")
+            assert.equal(translator.b2a("Prof Joyner of King's College, London."), "<span class=\"highlight\">Prof.</span> Joyner of King's College, London.")
         });
         test("Tea time is usually around 4 or 4.30.", function() {
-            assert.equal(translator.b2a("Tea time is usually around 4 or 4.30."), "Tea time is usually around 4 or 4:30.")
+            assert.equal(translator.b2a("Tea time is usually around 4 or 4.30."), "Tea time is usually around 4 or <span class=\"highlight\">4:30</span>.")
+        });
+    });
+    suite('Highlight', function() {
+        test("Mangoes are my favorite fruit.", function() {
+            const result = translator.a2b("Mangoes are my favorite fruit.");
+            assert.include(result, "<span class=\"highlight\">");
+        });
+        test("Yoghurt.", function() {
+            const result = translator.a2b("I ate yogurt for breakfast.");
+            assert.include(result, "<span class=\"highlight\">");
+        });
+        test("Soccer", function() {
+            const result = translator.b2a("We watched the footie match for a while.");
+            assert.include(result, "<span class=\"highlight\">");
+        });
+        test("Paracetamol", function() {
+            const result = translator.b2a("Paracetamol takes up to an hour to work.");
+            assert.include(result, "<span class=\"highlight\">");
         });
     })
 });
