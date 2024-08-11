b6aa1da573818c9c96198b013d21dfbcf1d29df0
Jan Konieczek <koleks92@gmail.com>
2024-08-11 08:26:22 +0000

    feat: a2b, b2a, capitalize, fixed reverseDict

--- components/translator.js
+++ components/translator.js
@@ -3,20 +3,19 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
-function capitalizeWord(text) {
-    const word = text.split("");
-    word[0] = word[0].toUpperCase();
-    return word.join("");
-  }
 
 function reverseDict(dictionary) {
     const reversedDict = {};
 
     for (const [key, value] of Object.entries(dictionary)) {
-    reversedDict[value] = key;
+        reversedDict[value] = key;  // Reverse the key-value pair
+    }
 
-    return reverseDict;
-    };
+    return reversedDict;  // Return the reversed dictionary after the loop completes
+};
+
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
 }
 
 class Translator {
@@ -24,89 +23,66 @@ class Translator {
     splitToArray(text) {
 
         const stringLowered = text.toLowerCase();
-
+        
         const stringsArray = stringLowered.split(" ");        
         
         return stringsArray;
-    
+        
     };
-
+    
     // American to British translation
     a2b(text) {
-        const array = this.splitToArray(text);
 
-        let newArray = [];
+        Object.keys(americanOnly).forEach(key => {
+            const regex = new RegExp(`\\b${key}\\b`, 'gi'); // word boundary to match full words only
+            text = text.replace(regex, americanOnly[key]);
+        });
+    
+        Object.keys(americanToBritishSpelling).forEach(key => {
+            const regex = new RegExp(`\\b${key}\\b`, 'gi');
+            text = text.replace(regex, americanToBritishSpelling[key]);
+        });
+    
+        Object.keys(americanToBritishTitles).forEach(key => {
+            // Create a regex to match the title with punctuation or end of string
+            const regex = new RegExp(key, 'gi');
+            text = text.replace(regex, capitalize(americanToBritishTitles[key]));
+          });
 
-        for (let i = 0; i < array.length; i++ ) {
+        // Time
+        text = text.replace(/(\d+):(\d+)/g, '$1.$2');
 
-            if (americanOnly[array[i].replace(/[.,?!]/g, '')]) {
-                 newArray.push(americanOnly[array[i].replace(/[.,?!]/g, '')])
-            } else {
-                if (americanToBritishSpelling[array[i].replace(/[.,?!]/g, '')]) {
-                    newArray.push(americanToBritishSpelling[array[i].replace(/[.,?!]/g, '')])
-               } else {
-                    if (americanToBritishTitles[array[i]]) {
-                        newArray.push(americanToBritishTitles[array[i]])
-                } else {
-                    newArray.push(array[i])
-                }
-               }
-            }
-        };
+        return text
+    }
 
-        newArray[0] = capitalizeWord(newArray[0]);
-
-        let newString = newArray.join(" ");
-
-        if ([".", "?", "!"].includes(newString[newString.length - 1])) {
-            return newString;
-        } else {
-            newString = newString + ".";
-            return newString
-        }
-        
-            
-
-    };
+    
 
     // British to American translation
     b2a(text) {
-        const array = this.splitToArray(text);
-
-        let newArray = [];
-
+        
         let britishToAmericanSpelling = reverseDict(americanToBritishSpelling);
         let britishToAmericanTitles = reverseDict(americanToBritishTitles);
 
 
+        Object.keys(britishOnly).forEach(key => {
+            const regex = new RegExp(`\\b${key}\\b`, 'gi'); // word boundary to match full words only
+            text = text.replace(regex, britishOnly[key]);
+        });
+    
+        Object.keys(britishToAmericanSpelling).forEach(key => {
+            const regex = new RegExp(`\\b${key}\\b`, 'gi');
+            text = text.replace(regex, britishToAmericanSpelling[key]);
+        });
+    
+        Object.keys(britishToAmericanTitles).forEach(key => {
+            const regex = new RegExp(`\\b${key}\\b`, 'gi');
+            text = text.replace(regex, capitalize(britishToAmericanTitles[key]));
+        });
 
-        for (let i = 0; i < array.length; i++ ) {
+        // Time
+        text = text.replace(/(\d+).(\d+)/g, '$1:$2');
 
-            if (britishOnly[array[i].replace(/[.,?!]/g, '')]) {
-                 newArray.push(britishOnly[array[i].replace(/[.,?!]/g, '')])
-            } else {
-                if (britishToAmericanSpelling[array[i].replace(/[.,?!]/g, '')]) {
-                    newArray.push(britishToAmericanSpelling[array[i].replace(/[.,?!]/g, '')])
-               } else {
-                    if (britishToAmericanTitles[array[i]]) {
-                        newArray.push(britishToAmericanTitles[array[i]])
-                } else {
-                    newArray.push(array[i])
-                }
-               }
-            }
-        };
-
-        newArray[0] = capitalizeWord(newArray[0])
-
-        let newString = newArray.join(" ");
-
-        if ([".", "?", "!"].includes(newString[newString.length - 1])) {
-            return newString;
-        } else {
-            newString = newString + ".";
-            return newString
-        }
+        return text
     }
 }
 
